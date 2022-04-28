0d66dde53e2943e62250b17d6646463158651e2a
bettinapaulucci <bettina.paulucci2@gmail.com>
2022-04-28 18:11:39 -0300

    contexto

--- src/Components/Utils/MostrarDetalle.js
+++ src/Components/Utils/MostrarDetalle.js
@@ -8,3 +8,14 @@ export const MostrarDetalle = () => {
             }, 300);
     });
     };
+
+    //export const MostrarDetalle = (id) => {
+        //return new Promise((resolve, reject) => {
+            //const productoElegido = productos.find(
+               // (producto) => producto.id === Number(id)
+           // );
+           // setTimeout(() => {
+               // resolve(productoElegido);
+            //}, 1000);
+        //});
+  //  };
